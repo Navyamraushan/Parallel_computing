5f256630047428c0afbb8b253e5c5ea190701803
bcs192 <raushan22bcs192@iiitkottayam.ac.in>
2024-09-27 15:16:50 +0530

    pi.cpp updated

--- pi.cpp
+++ pi.cpp
@@ -2,10 +2,23 @@
 #include <omp.h>
 using namespace std;
 
-static long num_steps = 10;
+static long num_steps = 10000;
 double step;
 
 // Reduction is used to combine the partial results of the threads, to avoid race conditions
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+static long num_steps = 10000;
+double step;
+
+// This code calculates the value of Pi using numerical integration (the midpoint rule).
+// The interval [0, 1] is divided into 'num_steps' subintervals, and the area under the curve
+// y = 4 / (1 + x^2) is approximated by summing up the areas of rectangles with width 'step'
+// and height '4 / (1 + x^2)' evaluated at the midpoint of each subinterval.
+// OpenMP is used to parallelize the for loop, and the 'reduction' clause is used to combine
+// the partial results of the threads to avoid race conditions.
 int main() {
     int i;
     double x, pi, sum = 0.0;
