87b1b304ea688f06433372f2f55fca2ed45f3bac
bcs192 <raushan22bcs192@iiitkottayam.ac.in>
2024-09-27 15:30:16 +0530

    changed previous code

--- pi.cpp
+++ pi.cpp
@@ -1,36 +1,45 @@
+/**
+ * @file pi.cpp
+ * @brief This program calculates the value of Pi using numerical integration and parallel computing with OpenMP.
+ *
+ * The program divides the task of calculating Pi into multiple threads to speed up the computation.
+ * It uses the midpoint rule for numerical integration to approximate the value of Pi.
+ *
+ * The main steps are:
+ * 1. Define the number of steps for the integration.
+ * 2. Calculate the width of each step.
+ * 3. Use OpenMP to parallelize the summation of the areas of rectangles under the curve.
+ * 4. Each thread calculates a partial sum of the areas.
+ * 5. The partial sums are combined using an atomic operation to avoid race conditions.
+ * 6. The final value of Pi is printed with a precision of 10 decimal places.
+ *
+ * @note The number of steps is set to 1,000,000,000 for a high-precision calculation.
+ * @note The OpenMP directives are used to parallelize the for loop and to ensure atomic updates to the shared variable `pi`.
+ */
 #include <iostream>
 #include <omp.h>
+#include <iomanip>
 using namespace std;
 
-static long num_steps = 10000;
-double step;
 
-// Reduction is used to combine the partial results of the threads, to avoid race conditions
-#include <iostream>
-#include <omp.h>
-using namespace std;
 
-static long num_steps = 10000;
-double step;
-
-// This code calculates the value of Pi using numerical integration (the midpoint rule).
-// The interval [0, 1] is divided into 'num_steps' subintervals, and the area under the curve
-// y = 4 / (1 + x^2) is approximated by summing up the areas of rectangles with width 'step'
-// and height '4 / (1 + x^2)' evaluated at the midpoint of each subinterval.
-// OpenMP is used to parallelize the for loop, and the 'reduction' clause is used to combine
-// the partial results of the threads to avoid race conditions.
 int main() {
-    int i;
-    double x, pi, sum = 0.0;
-    step = 1.0 / (double)num_steps;
+    const long num_steps = 1000000000;
+    double step = 1.0 / static_cast<double>(num_steps);
+    double pi = 0.0;
 
-    #pragma omp parallel for reduction(+:sum) private(x)
-    for (i = 0; i < num_steps; i++) {
-        x = (i + 0.5) * step;
-        sum += 4.0 / (1.0 + x * x);
+    #pragma omp parallel
+    {
+        double sum = 0.0;
+        #pragma omp for
+        for (long i = 0; i < num_steps; ++i) {
+            double x = (i + 0.5) * step;
+            sum += 4.0 / (1.0 + x * x);
+        }
+        #pragma omp atomic
+        pi += sum * step;
     }
 
-    pi = step * sum;
-    cout << "Value of Pi: " << pi << endl;
+    cout << std::fixed << std::setprecision(10) << "Calculated Pi: " << pi << endl;
     return 0;
 }
